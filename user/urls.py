67c8907567593eef4ee81c11616d3eece303fe1e
FelipeMassashi <felipemassashi@gmail.com>
2024-11-13 23:41:57 -0300

    fix: fixing code and requirements

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-from .views import UserView, VerifyTokenView
+from .views import UserView, VerifyTokenView, RegisterUserView
 from django.urls import path
 
 
@@ -7,5 +7,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('login', UserView.as_view(), name='login'),
-    path('verify_token', VerifyTokenView.as_view(), name='verify-token')
+    path('verify_token', VerifyTokenView.as_view(), name='verify-token'),
+    path('register', RegisterUserView.as_view(), name='register-user'),
 ]
