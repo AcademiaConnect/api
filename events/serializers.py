9cfdc8a8897caed11c19a2ec69e456b707e6b297
Victor Emanuel <78446120+p3rseus1@users.noreply.github.com>
2024-11-22 19:21:32 -0300

    Update serializers.py

--- events/serializers.py
+++ events/serializers.py
@@ -8,3 +8,15 @@ class EventSerializers(serializers.Serializer):
     location = serializers.CharField()
     description = serializers.CharField()
     user = serializers.CharField(required=False)
+
+    read_only_fields = ['id']  # Define o campo `id` como somente leitura
+
+    def update(self, instance, validated_data):
+        # Atualiza os campos da instância com os dados validados
+        instance.title = validated_data.get('title', instance.title)
+        instance.dateInitial = validated_data.get('dateInitial', instance.dateInitial)
+        instance.dateFinal = validated_data.get('dateFinal', instance.dateFinal)
+        instance.location = validated_data.get('location', instance.location)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()  # Salva a instância atualizada no banco de dados
+        return instance
